a0baf8d6cef294548c30d62e9cb2a79b0559ee32
dustinii <125335987+dustinii@users.noreply.github.com>
2023-08-08 22:37:56 -0700

    Create start.jsx

---
+++ start.jsx
@@ -0,0 +1,24 @@
+ <div>
+                <p>
+                    <hr>
+                    <br>
+                    I am a full-stack software developer equipped with comprehensive training from the University of
+                    California, Irvine from my undergrad, and, more recently, at the Full Stack Development
+                    program with the University of California, Los Angeles. My wide-ranging expertise spans numerous
+                    languages and technologies, allowing me to proficiently navigate both front-end and back-end
+                    frameworks.
+                    I am adept at delivering high-quality and
+                    efficient code, illustrated through diverse projects that
+                    I've spearheaded both independently and collaboratively.
+                    <br></br>
+                    My passion for building innovative solutions and tackling complex problems fuels my commitment to
+                    lifelong learning and growth which helps me to adapt
+                    and deliver in the ever-evolving landscape of Tech.
+                    I love building things and solving problems! This fuels my desire to contribute as an asset to
+                    cutting-edge
+                    projects. I am eager to find opportunities to
+                    collaborate, innovate, and make meaningful impacts in the dynamic field of technology.
+                    <br></br>
+                    <hr>
+                </p>
+            </div>
