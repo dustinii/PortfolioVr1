95913b654a3d9875bfbfac1b5b5d3b295531a138
dustinii <125335987+dustinii@users.noreply.github.com>
2023-09-12 00:27:30 -0700

    styling added

--- portfoliovr1/src/components/Contact/index.jsx
+++ portfoliovr1/src/components/Contact/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import './style.css';
 
 function Contact() {
   const [formData, setFormData] = useState({
